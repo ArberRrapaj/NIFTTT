855a231a1486e77c786102dded67a83f861a0a23
Arbër <Arber.Rrapaj@hotmail.de>
2019-06-13 18:27:42 -0400

    code cleanup updated swagger updated api image

--- backend/routes/apiGET.js
+++ backend/routes/apiGET.js
@@ -1,4 +1,4 @@
-module.exports = function(router, response, DB, Validator, Schemas) {
+module.exports = function (router, response, DB, Validator, Schemas) {
   /*
   router.get("/tokenTest", function (req, res) {
     console.log("inTokenTest");
@@ -9,17 +9,17 @@ module.exports = function(router, response, DB, Validator, Schemas) {
   }); */
 
   router.get("/", function (req, res) {
-   res.sendFile(path.join(__dirname, '../', 'api.png'))
-});
+    res.sendFile(path.join(__dirname, '../', 'api.png'))
+  });
 
-  router.get("/users/:email/rules", function(req, res) {
+  router.get("/users/:email/rules", function (req, res) {
     const validation = Validator.validate(req.params["email"], Schemas.email);
     if (validation.invalid) {
       response.badRequestError(validation.message, res);
       return;
     } else req.params["email"] = validation.value;
 
-    DB.query("SELECT *, (SELECT CASE WHEN COUNT(*) < 0 THEN 0 ELSE COUNT(*) END FROM RuleLogs as l WHERE r.id = l.ruleId) as runCount FROM Rules as r WHERE user = ?;", req.params["email"], function(err, result) {
+    DB.query("SELECT *, (SELECT CASE WHEN COUNT(*) < 0 THEN 0 ELSE COUNT(*) END FROM RuleLogs as l WHERE r.id = l.ruleId) as runCount FROM Rules as r WHERE user = ?;", req.params["email"], function (err, result) {
       if (err) {
         response.databaseError(res);
         console.log(err);
@@ -27,8 +27,8 @@ module.exports = function(router, response, DB, Validator, Schemas) {
     });
   });
 
-  router.get("/rules/:id", function(req, res) {
-    DB.query("SELECT * FROM Rules WHERE id = ?;", req.params["id"], function(err, result) {
+  router.get("/rules/:id", function (req, res) {
+    DB.query("SELECT * FROM Rules WHERE id = ?;", req.params["id"], function (err, result) {
       if (err) {
         response.databaseError(res);
         console.log(err);
@@ -36,8 +36,8 @@ module.exports = function(router, response, DB, Validator, Schemas) {
     });
   });
 
-  router.get("/platforms", function(req, res) {
-    DB.query("SELECT * FROM Platforms", function(err, result) {
+  router.get("/platforms", function (req, res) {
+    DB.query("SELECT * FROM Platforms", function (err, result) {
       if (err) {
         console.log(err);
         response.databaseError(res);
