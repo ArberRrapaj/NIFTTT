edfd3efe59da0ebbef0774a4423b3274f0486fd2
Bengin Cetindere <bengin@cetindere.de>
2019-06-14 14:54:04 +0200

    email fix

--- backend/bin/eventChecker.js
+++ backend/bin/eventChecker.js
@@ -122,7 +122,7 @@ module.exports = function (DB) {
                 break;
         }
         transporter.sendMail({
-            from: '"NIFTTT 🐬" <niftt@cetindere.com>', // sender address
+            from: '"NIFTTT" <nifttt@cetindere.com>', // sender address
             to: rule.actionPayload, // list of receivers
             subject: "NIFTTT Rule triggered", // Subject line
             text: message, // plain text body
